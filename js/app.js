6e12bd8a45a2318469d8ef02742cb1d9494080f7
Festival3224 <QSR^V9&h>
2026-04-24 17:46:07 +0200

    init project structure

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import { state } from './state.js';
+
+console.log('App started');
+console.log(state);
---
+++ js/state.js
@@ -0,0 +1,6 @@
+export const state = {
+  currentMonth: new Date().getMonth(),
+  currentYear: new Date().getFullYear(),
+  currentView: 'projects',
+  monthlyData: {},
+};
